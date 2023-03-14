663a7149e0a6eb73c5751d86e3c47b25adbd38f4
sanjay kumar <sanjaykumar034831@gmail.com>
2023-03-14 23:31:08 +0500

    completed programming for boolean retrival model (simple and proximity)

---
+++ BooleanRetrival.js
@@ -0,0 +1,220 @@
+const natural = require('natural');
+const fs = require('fs');
+const readline = require('readline');
+
+const io = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+const operators = ["and", "or", "not"];
+
+const tokenizer = new natural.WordTokenizer();
+
+// step 1_clean document for 
+const clean_doc = (doc) => {
+    const symbols_replace = "\t\n;?:!.,—";
+    const symbols_remove = "!.'’:@#$%^&?<>*“”()[}{]-=;/\"\\";
+    let clean_doc = doc;
+    for (let i = 0; i < symbols_remove.length; i++) {
+        const symbol = symbols_remove[i];
+        const replace_by_space = symbols_replace.includes(symbol);
+        clean_doc = clean_doc.replaceAll(symbol, replace_by_space ? ' ' : '');
+    }
+    return clean_doc;
+}
+
+// step 2_ preprocess doc for case folding, removal of stop words and stemming over tokens
+const pre_process_docs = (text) => {
+    const stop_words = fs.readFileSync('Stopword-List.txt', 'utf-8').split('\n');
+    const tokens = tokenizer.tokenize(text);
+    // case folding
+    const case_fold_tokens = tokens.map((token) => {
+        return token.toLowerCase();
+    })
+    // stop words removal
+    const filtered_tokens = case_fold_tokens.filter((token) => {
+        return !stop_words.includes(token);
+    })
+
+    // porter stemming over tokens
+    const porter_stemmer = natural.PorterStemmer;
+    const stem_tokens = filtered_tokens.map((token) => {
+        return porter_stemmer.stem(token);
+    })
+    return stem_tokens;
+}
+// step3_ create inverted index with positions 
+let inverted_positions = new Map();
+const dataset = fs.readdirSync('Dataset');
+
+dataset.forEach((datafile) => {
+
+    const unique_file_id = parseInt(datafile.split(".")[0]);
+    const file = fs.readFileSync(`Dataset/${datafile}`, 'utf-8');
+    const cleaned_doc = clean_doc(file);
+    const tokens = pre_process_docs(cleaned_doc);
+    tokens.forEach((token, position) => {
+
+        if (!inverted_positions.has(token)) {
+            let docToPosition = new Map();
+            let position_set = new Set();
+
+            position_set.add(position);
+            docToPosition.set(unique_file_id, position_set);
+            inverted_positions.set(token, docToPosition);
+        } else {
+            let existing_token = inverted_positions.get(token);
+            if (!existing_token.has(unique_file_id)) {
+                let position_set = new Set();
+                position_set.add(position);
+                existing_token.set(unique_file_id, position_set);
+            } else {
+                let position_set = existing_token.get(unique_file_id);
+                position_set.add(position);
+            }
+        }
+
+    });
+})
+
+// finding intersection of two operands from query 
+const intersection_map = (operand_one, operand_two) => {
+    const intersection = new Map();
+    operand_one.forEach((value, key) => {
+        if (operand_two.has(key)) {
+            intersection.set(key, value);
+        }
+    });
+    return intersection;
+};
+
+// finding union of two operands from query 
+const union_map = (operand_one, operand_two) => {
+    const union = new Map(operand_one);
+    operand_two.forEach((value, key) => {
+        if (!union.has(key)) {
+            union.set(key, value);
+        }
+    });
+    return union;
+};
+
+// finding difference of two operands from query 
+const difference_map = (operand_one, operand_two) => {
+    const difference = new Map(operand_one);
+    operand_two.forEach((value, key) => {
+        if (difference.has(key)) {
+            difference.delete(key);
+        }
+    });
+    return difference;
+};
+
+const get_operands = (query_tokens) => {
+    return query_tokens.filter(
+        (value) => !operators.includes(value.toLowerCase())
+    );
+}
+const get_operators = (query_tokens) => {
+    return query_tokens.filter((value) =>
+        operators.includes(value.toLowerCase())
+    );
+}
+const get_token_maps = (query_operands) => {
+    return query_operands.map(
+        (value) => inverted_positions.get(value) || new Map()
+    );
+}
+
+// solving proximity query having /
+const solve_proximity_query = (query) => {
+    const stemmer = natural.PorterStemmer;
+    const query_tokens = tokenizer.tokenize(query);
+    const operand_one = stemmer.stem(query_tokens[0]);
+    const operand_two = stemmer.stem(query_tokens[1]);
+    const proximity_distance = parseInt(query_tokens[2]);
+    const posting_list_one = inverted_positions.get(operand_one);
+    const posting_list_two = inverted_positions.get(operand_two);
+    if (!posting_list_one || !posting_list_two){
+        console.log([]);
+        return;
+    }
+    
+    const result = new Map();
+    for (const [doc_id_left, pos_set_left] of posting_list_one.entries()) {
+        for (const [doc_id_right, pos_set_right] of posting_list_two.entries()) {
+            if (doc_id_left === doc_id_right) {
+                const doc_positions = new Map();
+                for (const pos_left of pos_set_left) {
+                    for (const pos_right of pos_set_right) {
+                        if (Math.abs(pos_right - pos_left) <= proximity_distance && pos_right - pos_left > 0) {
+                            doc_positions.set(pos_left, pos_right);
+                        }
+                    }
+                }
+                if (doc_positions.size !== 0) {
+                    result.set(doc_id_left, doc_positions);
+                }
+            }
+        }
+    }
+
+    console.log([...result.keys()].sort((a, b) => a - b));
+
+}
+
+
+// solving simple boolean query with and, or , not operators. 
+const solve_query = (query) => {
+    const query_tokens = tokenizer.tokenize(query);
+    const porter_stemmer = natural.PorterStemmer;
+    
+    const operands = get_operands(query_tokens);
+    const query_operands = operands.map((token) => {
+        return porter_stemmer.stem(token);
+    })
+    const query_operators = get_operators(query_tokens);
+    const token_maps = get_token_maps(query_operands);
+
+    const map = token_maps.reduce((acc, curr, index) => {
+        const operator = query_operators[index - 1];
+        if (operator === "AND" || operator === "and") {
+            return intersection_map(acc, curr);
+        } else if (operator == "OR" || operator === "or") {
+            return union_map(acc, curr);
+        } else if (operator == "NOT" || operator === "not") {
+            return difference_map(acc, curr);
+        } else {
+            return acc;
+        }
+    });
+
+    console.log([...map.keys()].sort((a, b) => a - b));
+};
+
+function askQuery() {
+    io.question('Select from query (or 0 to exit): \n 1. Simple Query \n 2. Proximity Query\n Select: ', (answer) => {
+        if (answer === '0') {
+            io.close();
+            return;
+        }
+        if (answer === '1') {
+            io.question('Enter simple query : ', (query) => {
+                solve_query(query);
+                askQuery();
+            })
+        } else if (answer === '2') {
+            io.question('Enter proximity query : ', (query) => {
+                solve_proximity_query(query);
+                askQuery();
+            })
+        } else {
+            console.log("Invalid Input");
+            askQuery();
+        }
+    });
+}
+
+askQuery();
